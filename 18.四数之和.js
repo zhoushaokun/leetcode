0309e1a9ca9ff237031aca04c6a396ea0c9f4839
周少坤 <zhoushaokunixx@163.com>
2020-01-04 11:57:34 +0800

    第二部分遍历有问题（待解决）

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -12,25 +12,29 @@
  */
 var fourSum = function(nums_o, target) {
     if (nums_o.length < 4) return [];
-    const nums = [...nums_o].sort();
+    const nums = [...nums_o].sort((a, b) => a - b);
+    console.log('nums', nums)
     let twoSum = new Map();
+    let res = [];
+
     for (let i = 0; i < nums.length; ) {
-        while (nums[i] === nums[++i]) {}
-        for (let j = i + 1; j < nums.length; j++) {
-            while (nums[j] === nums[++j]) {}
+        for (let j = i + 1; j < nums.length; ) {
             twoSum.set([i, j], nums[i]+nums[j]);
+            while (nums[j] === nums[++j]) { }
         }
+        while (nums[i] === nums[++i]) { }
     }
 
-    let m = {};
-    let res = [];
+    console.log('twoSum', twoSum);
+    
     for (const [key, value] of twoSum) {
-        if(m[target-value]) {
-            
-        } else {
-            m[value] = key;
+        for (const [key_s, value_s] of twoSum) {
+            if(value + value_s === target && new Set([...key_s, ...key]).size === key.length + key_s.length) {
+                res.push([...key_s, ...key]);
+            }
         }
     }
+    return res;
 };
 // @lc code=end
 
