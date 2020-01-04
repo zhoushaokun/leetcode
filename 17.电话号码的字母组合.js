85f429328d1f0b4ec10a4ae8dbcba32ea6116e2a
周少坤 <zhoushaokunixx@163.com>
2020-01-04 10:56:29 +0800

    完成电话号码的字母组合（递归做法）

--- 17.电话号码的字母组合.js
+++ 17.电话号码的字母组合.js
@@ -9,8 +9,38 @@
  * @param {string} digits
  * @return {string[]}
  */
+
+ const numberMapLetter =  {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+ };
+ 
 var letterCombinations = function(digits) {
-    
+    if (digits.length === 0) return [];
+    return combineOne(numberMapLetter[digits[0]], digits.substring(1));
 };
+
+function combineOne(beforeResult, subDigits) {
+    if (subDigits.length === 0) {
+        return beforeResult;
+    } else {
+        let firstLetter = subDigits[0];
+        const result = numberMapLetter[firstLetter].reduce((pre, cur, index) => {
+            return pre.concat(beforeResult.map((item) => {
+                return item + cur;
+            }));
+        }, []);
+
+        return combineOne(result, subDigits.substring(1));
+    }
+}
+
+
 // @lc code=end
 
