987be7715ab3024091ea61af8611439bc61d52ab
周少坤 <zhoushaokunixx@163.com>
2020-01-04 16:49:47 +0800

    0000 解决不了

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -17,24 +17,37 @@ var fourSum = function(nums_o, target) {
     let twoSum = new Map();
     let res = [];
 
-    for (let i = 0; i < nums.length; ) {
+    for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; ) {
             twoSum.set([i, j], nums[i]+nums[j]);
             while (nums[j] === nums[++j]) { }
         }
-        while (nums[i] === nums[++i]) { }
+        // while (nums[i] === nums[++i]) { }
     }
-
+    
     console.log('twoSum', twoSum);
     
     for (const [key, value] of twoSum) {
         for (const [key_s, value_s] of twoSum) {
+            console.log('[...key, ...key_s].sort().join(",")', [...key, ...key_s].sort().join(","));
+            if(res.indexOf([...key, ...key_s].sort().join(",")) > -1) {
+                continue;
+            }
             if(value + value_s === target && new Set([...key_s, ...key]).size === key.length + key_s.length) {
-                res.push([...key_s, ...key]);
+                console.log('key', key);
+                console.log('key_s', key_s);
+                
+                res.push([...key, ...key_s].sort().join(","));
             }
         }
     }
-    return res;
+
+    console.log('res', res);
+    return res.map((item) => {
+        let item_a = item.split(",").map(item => Number(item));
+        console.log('item_a', item_a);
+        return [nums[item_a[0]], nums[item_a[1]], nums[item_a[2]], nums[item_a[3]]];
+    });
 };
 // @lc code=end
 
