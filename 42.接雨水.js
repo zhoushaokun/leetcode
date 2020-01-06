5f912f6e7d8796b567e3fc13c3fb6af3ce20ab83
周少坤 <zhoushaokunixx@163.com>
2020-01-06 20:12:46 +0800

    完成暴力破解法求解接雨水问题

--- 42.接雨水.js
+++ 42.接雨水.js
@@ -11,6 +11,24 @@
  */
 var trap = function(height) {
     
+  if(height.length < 3) return 0;
+  let res = 0;
+  for (let i = 1; i < height.length - 1; i++) {
+    const p = height[i];
+    let height_left = p;
+    let height_right = p;
+    for (let j = i-1; j >= 0; j--) {
+      height_left = Math.max(height_left, height[j]);
+    }
+    
+    for (let k = i + 1; k < height.length; k++) {
+      height_right = Math.max(height_right, height[k]);
+    }
+    res += Math.min(height_right, height_left) - p;
+    console.log('res', res)
+  }
+  return res;
+    
 };
 // @lc code=end
 
