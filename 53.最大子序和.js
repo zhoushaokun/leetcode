7605a5f3b317995e9ce760bcef5ee25082be3a3c
zhoushaokun <1655040956@qq.com>
2020-02-20 15:43:31 +0800

    完成最大子序和

---
+++ 53.最大子序和.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let ans = nums[0];
+    let sum = 0;
+    for (const num of nums) {
+        if (sum + num > num) {
+            sum += num;
+        } else {
+            sum = num;
+        }
+        ans = Math.max(ans, sum);
+    }
+    return ans;
+};
+// @lc code=end
+
