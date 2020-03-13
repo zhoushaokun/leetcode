af90308435c149c9ecfa7deb22d7c428f9dcd262
zhoushaokun <1655040956@qq.com>
2020-03-13 21:49:06 +0800

    二叉树的层次遍历

--- 102.二叉树的层次遍历.js
+++ 102.二叉树的层次遍历.js
@@ -16,8 +16,37 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// BFS的做法
 var levelOrder = function(root) {
+    if(!root) return [];
+    const stack = [root];
+    const res = [];
+    let num = 1;
+    let counter = 0;
 
+    let oneLayer = [];
+    while (stack.length) {
+        let p = stack.shift();
+        num --;
+        oneLayer.push(p.val);
+        if(p.left) {
+            stack.push(p.left);
+            counter ++;
+        }
+        if(p.right) {
+            stack.push(p.right);
+            counter ++;
+        }
+        if(num === 0) {
+            num = counter;
+            counter = 0;
+            res.push(oneLayer);
+            oneLayer = [];
+        }
+    }
+    return res;
 };
+
+// DFS 的做法，给每个节点标志一个深度
 // @lc code=end
 
